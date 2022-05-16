b0d260976d78cf1f4606adaae18e5e9b1ff4990c
enonnemacher-ciandt <enonnemacher@ciandt.com>
2022-05-16 15:44:10 -0300

    Refact task 1-Print Integer Variable

--- 0x00-java-hello_world/PrintIntegerVariable.java
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -1,6 +1,6 @@
 public class PrintIntegerVariable {
     public static void main(String[] args) {
         int numero = 311;
-        System.out.printf("Avenida Andaló, %d", numero);
+        System.out.printf("Avenida Andaló, %d\n", numero);
     }
 }
