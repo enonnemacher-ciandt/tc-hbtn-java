f07751883cd2592db9aa8ebef5346ca38e0bf7c9
enonnemacher-ciandt <enonnemacher@ciandt.com>
2022-05-16 16:02:34 -0300

    Refact task 2-Print Float Variable

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,8 +1,5 @@
-import java.util.Locale;
-
 public class PrintFloatVariable {
     public static void main(String[] args) {
-        Locale.setDefault(new Locale("en", "US"));
         final float PI = 3.14159f;
         System.out.printf("Valor de pi: %.3f\n", PI);
     }
