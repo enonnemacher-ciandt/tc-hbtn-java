1e8bb49b4ea258eb75d9497094066cd45e8ff4ec
enonnemacher-ciandt <enonnemacher@ciandt.com>
2022-05-17 14:21:46 -0300

    Task 0-Personagem Game

---
+++ 0x04/personagem/PersonagemGame.java
@@ -0,0 +1,24 @@
+public class PersonagemGame {
+
+    public String nome;
+    public int saudeAtual;
+
+    public PersonagemGame() {
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+}
