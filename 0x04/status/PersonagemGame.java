b026988dc33ed63fd6d8ba677056f2f0e39b0335
enonnemacher-ciandt <enonnemacher@ciandt.com>
2022-05-17 14:54:15 -0300

    Task 3-Status do Personagem

---
+++ 0x04/status/PersonagemGame.java
@@ -0,0 +1,43 @@
+public class PersonagemGame {
+
+    private String nome;
+    private int saudeAtual;
+    private String status;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if (this.saudeAtual > 0) {
+            this.status = "vivo";
+        } else if (this.saudeAtual == 0) {
+            this.status = "morto";
+        }
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        setSaudeAtual(getSaudeAtual() - quantidadeDeDano);
+        if (getSaudeAtual() < 0)
+            setSaudeAtual(0);
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        setSaudeAtual(getSaudeAtual() + quantidadeDeCura);
+        if (getSaudeAtual() > 100)
+            setSaudeAtual(100);
+    }
+}
