ad17aa395eb674f4dd563d76975f83e42f1ab202
enonnemacher-ciandt <enonnemacher@ciandt.com>
2022-05-23 16:45:41 -0300

    Task 1-Forma Geometrica

---
+++ 0x05/forma/FormaGeometrica.java
@@ -0,0 +1,8 @@
+package task1formageometrica;
+
+public class FormaGeometrica {
+
+    public double area(){
+        throw new UnsupportedOperationException("Funcao area nao implementada");
+    }
+}
