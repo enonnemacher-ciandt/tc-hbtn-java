7366ffb5faee76c95015fddf2aff2955b417c0ff
enonnemacher-ciandt <enonnemacher@ciandt.com>
2022-05-30 16:31:39 -0300

    Task 1-Cookies

---
+++ List/cookies/Pedido.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pedido {
+
+    private List<PedidoCookie> cookies;
+
+    public Pedido() {
+        this.cookies = new ArrayList<>();
+    }
+
+    public void adicionarPedidoCookie(PedidoCookie pedidoCookie) {
+        cookies.add(pedidoCookie);
+    }
+
+    public int obterTotalCaixas() {
+        int quantidade = 0;
+        for (PedidoCookie pedido : cookies) {
+            quantidade += pedido.getQuantidadeCaixas();
+        }
+        return quantidade;
+    }
+
+    public int removerSabor(String sabor) {
+        int quantidade = 0;
+        for (PedidoCookie pedido : cookies) {
+            if (pedido.getSabor().equalsIgnoreCase(sabor)) {
+                quantidade += pedido.getQuantidadeCaixas();
+            }
+        }
+
+        cookies.removeIf(pedidoCookie -> pedidoCookie.getSabor().equalsIgnoreCase(sabor));
+
+        return quantidade;
+    }
+}
