0fa9c5949d22e2e4fd124972eeccf922b95755fb
enonnemacher-ciandt <enonnemacher@ciandt.com>
2022-05-31 08:32:09 -0300

    Task 2-Lista TODO

---
+++ List/todo/ListaTodo.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTodo {
+
+    private List<Tarefa> tarefas;
+
+    public ListaTodo() {
+        this.tarefas = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(Tarefa novaTarefa) {
+        for (Tarefa listaDeTarefas : tarefas) {
+            if (novaTarefa.getIdentificador() == listaDeTarefas.getIdentificador()) {
+                throw new IllegalArgumentException("Tarefa com identificador " + novaTarefa.getIdentificador() + " ja existente na lista");
+            }
+        }
+        tarefas.add(novaTarefa);
+    }
+
+    public boolean marcarTarefaFeita(int identificador) {
+        boolean tarefaAlterada = false;
+        for (Tarefa listaDeTarefas : tarefas) {
+            if (listaDeTarefas.getIdentificador() == identificador) {
+                listaDeTarefas.setEstahFeita(true);
+                tarefaAlterada = true;
+            }
+        }
+        return tarefaAlterada;
+    }
+
+    public boolean desfazerTarefa(int identificador) {
+        boolean tarefaAlterada = false;
+        for (Tarefa listaDeTarefas : tarefas) {
+            if (listaDeTarefas.getIdentificador() == identificador) {
+                listaDeTarefas.setEstahFeita(false);
+                tarefaAlterada = true;
+            }
+        }
+        return tarefaAlterada;
+    }
+
+    public void desfazerTodas() {
+        for (Tarefa listaDeTarefas : tarefas) {
+            listaDeTarefas.setEstahFeita(false);
+        }
+    }
+
+    public void fazerTodas() {
+        for (Tarefa listaDeTarefas : tarefas) {
+            listaDeTarefas.setEstahFeita(true);
+        }
+    }
+
+    public void listarTarefas() {
+        for (Tarefa listaDeTarefas : tarefas) {
+            if (listaDeTarefas.isEstahFeita()) {
+                System.out.print("[X]");
+            } else {
+                System.out.print("[ ]");
+            }
+            System.out.println("  Id: " + listaDeTarefas.getIdentificador() + " - Descricao: " + listaDeTarefas.getDescricao());
+        }
+    }
+}
