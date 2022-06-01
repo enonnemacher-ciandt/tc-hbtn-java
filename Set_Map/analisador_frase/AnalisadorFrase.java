ce4f9cb6a167adc61a119924397d5bbe42673fc2
enonnemacher-ciandt <enonnemacher@ciandt.com>
2022-06-01 14:38:19 -0300

    Refact task 0-Contagem de Palavras

--- Set_Map/analisador_frase/AnalisadorFrase.java
+++ Set_Map/analisador_frase/AnalisadorFrase.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 public class AnalisadorFrase {
 
-    public static TreeMap contagemPalavras(String frase) {
+    public static TreeMap<String, Integer> contagemPalavras(String frase) {
         TreeMap<String, Integer> listaDeOcorrencias = new TreeMap<String, Integer>(); // chave é a palavra, valor é a quantidade de ocorrencias
 
         // transforma em minusculas, remove espaços antes, depois, caracteres especiais ? . !, e coloca as palavras na lista
-        List<String> listaDePalavras = Arrays.asList(frase.toLowerCase().trim().split("[.!?] *| +"));
+        String[] listaDePalavras = frase.toLowerCase().trim().split("[.!?] *| +");
 
         for (String item : listaDePalavras) { // itera sobre o vetor de palavras
             Integer ocorrencia = listaDeOcorrencias.get(item); // busca o valor da chave no TreeMap, se não existir, retorna null
