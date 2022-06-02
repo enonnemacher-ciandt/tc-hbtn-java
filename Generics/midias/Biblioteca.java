0c7a22ace8fbac4a8f7c265314f64f475183ce27
enonnemacher-ciandt <enonnemacher@ciandt.com>
2022-06-02 11:27:07 -0300

    Task 0-Midias

---
+++ Generics/midias/Biblioteca.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca<T> {
+
+    private List<Midia> lista = new ArrayList<>();
+
+    public void adicionarMidia(Midia valor){
+        lista.add(valor);
+    }
+
+    public List<Midia> obterListaMidias(){
+        return this.lista;
+    }
+}
