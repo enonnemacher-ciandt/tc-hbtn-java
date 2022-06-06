f3fb7dd4da4370a4b35d9fd66eea5ad9e70d1d48
enonnemacher-ciandt <enonnemacher@ciandt.com>
2022-06-06 16:55:57 -0300

    Task 1-Area Quadrado

---
+++ 0x02/quadrado/Quadrado.java
@@ -0,0 +1,10 @@
+public class Quadrado {
+
+    public static double area(double lado) {
+        if (lado < 0) {
+            throw new java.lang.IllegalArgumentException("Lado deve possuir valor positivo");
+        }
+
+        return lado * lado;
+    }
+}
