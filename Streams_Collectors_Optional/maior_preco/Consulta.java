232678946a7cc597419520330db6941ea30aaa84
enonnemacher-ciandt <enonnemacher@ciandt.com>
2022-06-13 15:50:20 -0300

    Task 1-Relatorio Produto com maior preco

---
+++ Streams_Collectors_Optional/maior_preco/Consulta.java
@@ -0,0 +1,23 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Consulta {
+
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        List<Produto> listaLivros = pedido
+                .getProdutos()
+                .stream()
+                .filter(produto -> produto.getCategoria().equals(CategoriaProduto.LIVRO))
+                .collect(Collectors.toList());
+
+        return listaLivros;
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos
+                .stream()
+                .max(Comparator.comparing(Produto::getPreco))
+                .get();
+    }
+}
