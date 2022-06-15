7ec9a3711cd5f9482e6ff82c1d81612c3d6b9a3f
enonnemacher-ciandt <enonnemacher@ciandt.com>
2022-06-15 15:23:13 -0300

    Task 0-Serializacao de Estudantes

---
+++ Serializacao/serializacao/Estudante.java
@@ -0,0 +1,19 @@
+import java.io.Serializable;
+
+public class Estudante implements Serializable {
+
+    private int idade;
+    private String nome;
+    transient private String senha;
+
+    public Estudante(int idade, String nome, String senha) {
+        this.idade = idade;
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudante { nome='" + this.nome + "', idade='" + this.idade + "', senha='" + this.senha + "' }";
+    }
+}
---
+++ Serializacao/serializacao/SerializarEstudantes.java
@@ -0,0 +1,40 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SerializarEstudantes<Estudante> {
+
+    private String nomeArquivo;
+
+    public SerializarEstudantes(String nomeArquivo) {
+        this.nomeArquivo = nomeArquivo;
+    }
+
+    public void serializar(List<Estudante> listaEstudantes) {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(this.nomeArquivo);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(listaEstudantes);
+            objectOutputStream.close();
+        } catch (Exception e) {
+            System.out.println("Nao foi possivel serializar");
+        }
+    }
+
+    public List<Estudante> desserializar() {
+        List<Estudante> listaEstudantes = new ArrayList<>();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(this.nomeArquivo);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            listaEstudantes = (List<Estudante>) objectInputStream.readObject();
+            objectInputStream.close();
+        } catch (Exception e) {
+            System.out.println("Nao foi possivel desserializar");
+        }
+
+        return listaEstudantes;
+    }
+}
